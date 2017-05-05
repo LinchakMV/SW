c7e11bf07b151e8c2b56eb939a394582e704d06a
linchakmv <linchakmv@gmail.com>
2017-05-05 23:53:30 +0300

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 import sayHello from './lib/sayHello.js';
-
 sayHello();
+
+// $(document).ready(function() {
+//       $('.slider').slick({
+//         infinite: true,
+//         // autoplay: false,
+//         speed: 300,
+//         slidesToShow: 1,
+//         // lazyLoad: 'progressive',
+//         arrows: true,
+//         // slidesToScroll: 1,
+//         dots: false,
+//       });
+//     });
